003f1972391f7e10de30e4d7dd8ac01edecc2189
Jangle Mangle <Jangle@mangle.com>
2024-06-18 23:34:50 +0530

    Added new line to Main.java

--- Main.java
+++ Main.java
@@ -5,5 +5,6 @@ public static void main(String[] args) {
     System.out.println("Hello java!");
     System.out.println(new Date());
     System.out.println("jenkins in java");
+    System.out.println("jenkins new program in java");
 }
 }
