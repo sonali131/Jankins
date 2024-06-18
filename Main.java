f41d32fcf7ead524a2b97c5ca52872e67384ee8d
Jangle Mangle <Jangle@mangle.com>
2024-06-18 22:49:26 +0530

    first commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class Main{
+public static void main(String[] args) {
+    System.out.println("Hello java!");
+    System.out.println(new Date());
+    System.out.println("jenkins in java");
+}
+}
