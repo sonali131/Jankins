f5c72ca7c3bef616ca736a2e8e57b17e3cb416d3
Jangle Mangle <Jangle@mangle.com>
2024-06-18 23:45:12 +0530

    Added new Student to Student.java

---
+++ Student.java
@@ -0,0 +1,12 @@
+public class Student {
+    public Student(){
+        System.out.println("Creating object");
+    }
+    public void sayhello(){
+     System.out.println("I am a student");
+    }
+    public static void main(String[] args) {
+        Student s=new Student();
+        s.sayhello();
+    }
+}
